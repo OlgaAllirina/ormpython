1c958f9edc25cc10cbc042f85159228781df1351
Olga Menuhova <wowolgaivanova@gmail.com>
2024-01-20 17:20:54 +0300

    update file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlalchemy
+import psycopg2
 from sqlalchemy.orm import sessionmaker
-from model import create_table, Stok, Sale, Shop, Book, Publisher
+from model import create_table, Stock, Sale, Shop, Book, Publisher
 
 sql_sys = "postgresql"
 login = "postgres"
@@ -11,15 +12,46 @@ DSN = f'{sql_sys}://{login}:{password}@localhost:{host}/{sql_base}'
 
 engine = sqlalchemy.create_engine(DSN)
 
+# создадим таблицу
+create_table(engine)
+# заполним таблицу данными
+
+publisher1 = Publisher(name="Татьяна", books=[
+    Book(title="Не буди ведьму"),
+    Book(title="Ведьмин круг"),
+    Book(title="Беги, ведьма")])
+publisher2 = Publisher(name="Карен", books=[
+    Book(title="Тайны рукописи"),
+    Book(title="В оковах льда"),
+    Book(title="Рождённая огнём")])
+b1 = Book(title="Светочи тьмы", publisher=publisher1)
+b2 = Book(title="Цербер-хранитель", publisher=publisher1)
+b3 = Book(title="Прикосновения теней", publisher=publisher2)
+
+shop1 = Shop(name="Читай-город")
+stock1 = Stock(book=b1, shop=shop1, count="50")
+sale1 = Sale(price="540", stock=stock1, date_sale="15.09.2023", count="5")
+stock2 = Stock(book=b2, shop=shop1, count="35")
+sale2 = Sale(price="670", stock=stock1, date_sale="12.01.2020", count="12")
+
+shop2 = Shop(name="ДомКниг")
+stock3 = Stock(book=b3, shop=shop2, count="27")
+sale3 = Sale(price="472", stock=stock2, date_sale="31.05.2007", count="30")
+stock4 = Stock(book=b2, shop=shop2, count="33")
+sale4 = Sale(price="347", stock=stock2, date_sale="10.03.2008", count="9")
+
 Session = sessionmaker(bind=engine)
 session = Session()
+session.add_all([publisher1, publisher2, shop1, shop2, stock1, stock2, stock3, stock4, sale1, sale2, sale3, sale4])
+session.commit()
 
-create_table(engine) # создадим таблицу
+name_or_id = input("Введите имя или id автора: ")
+if name_or_id.isdigit():
+    query = session.query(Book).join(Stock).join(Shop).join(Sale).join(Publisher).filter(Publisher.id == name_or_id).all()
+
+
+# "название книги | название магазина, в котором была куплена эта книга | стоимость покупки | дата покупки"
+
+
 session.close()
-
-if __name__ == '__main__':
-    pass
-
-
-
 
--- model.py
+++ model.py
@@ -10,8 +10,6 @@ class Publisher(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=60), unique=True)
 
-    books = relationship("Book", backref="publishers")
-
     def __str__(self):
         return f'{self.id} : {self.name}'
 
@@ -23,27 +21,10 @@ class Book(Base):
     title = sq.Column(sq.String(length=50), unique=True)
     id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
 
-    publishers = relationship(Publisher, backref="books")
-    stoks = relationship("Stok", backref="books")
+    publisher = relationship(Publisher, backref="books")
 
     def __str__(self):
-        return f'{self.id} : ({self.title}, {self.id_publisher})'
-
-
-class Stok(Base):
-    __tablename__ = "stok"
-
-    id = sq.Column(sq.Integer, primary_key=True)
-    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
-    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
-    count = sq.Column(sq.Integer, nullable=False)
-
-    books = relationship("Book", backref="stoks")
-    shops = relationship("Shop", backref="stoks")
-    sales = relationship("Sale", backref="stoks")
-
-    def __str__(self):
-        return f'{self.id} : ({self.id_book}, {self.id_shop}, {self.count})'
+        return f'{self.title}'
 
 
 class Shop(Base):
@@ -52,25 +33,38 @@ class Shop(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=60), unique=True)
 
-    stoks = relationship("Stok", backref="shops")
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Stock(Base):
+    __tablename__ = "stock"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+
+    shop = relationship(Shop, backref="stock1")
+    book = relationship(Book, backref="stocks")
 
     def __str__(self):
-        return f'{self.id} : {self.name}'
+        return f'{self.count}'
 
 
 class Sale(Base):
     __tablename__ = "sale"
 
     id = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Integer, nullable=False)
-    date_sales = sq.Column(sq.Integer, nullable=False)
-    id_stok = sq.Column(sq.Integer, sq.ForeignKey("stok"), nullable=False)
+    price = sq.Column(sq.Float, nullable=False)
+    date_sale = sq.Column(sq.Date, nullable=False)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
 
-    stoks = relationship("Stok", backref="sales")
+    stock = relationship(Stock, backref="stocks")
 
     def __str__(self):
-        return f'{self.id} : ({self.price}, {self.date_sales}, {self.count}, {self.stok})'
+        return f'| {self.price} | {self.date_sale}'
 
 
 def create_table(engine):
